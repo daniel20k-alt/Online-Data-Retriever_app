4e7dffde63579ecd8e8379ccee51c864d69e3f4e
Testing_author <testing_beta@mail.com>
2020-02-20 23:06:15 +0200

    Using async() to push code to the background thread, and then to push it back to the main thread, therefore the UI would not be blocked

--- Online Data Retriever_app/ViewController.swift
+++ Online Data Retriever_app/ViewController.swift
@@ -13,47 +13,56 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
-//        let urlString = "https://www.api.whitehouse.gov/v1/petitions.json?limit=100"
+        
         let urlString: String
-            
+        
         if navigationController?.tabBarItem.tag == 0 {
-urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-            parse(json: data)
-         return
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(json: data)
+                    return
+                }
             }
+            self?.showError()
         }
-            showError()
     }
     
     func showError() {
+        DispatchQueue.main.async { [weak self] in
+        
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading your feed; please check your connection and try again", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Ok", style: .default))
-        present(ac, animated: true)
+            self?.present(ac, animated: true)
     }
-
+    }
+    
+    
     func parse(json: Data) {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//                tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        //                tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let petition = petitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
